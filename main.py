004e69679a920c317d6364dfc7c26c418e020fa7
FII14 <121723975+FII14@users.noreply.github.com>
2023-08-06 08:59:16 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import time
+import zipfile
+import os
+import sys
+
+print("""
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@ Program : Pemecah Kata Sandi File Zip     @
+@ Pembuat : Rofi [FII14]                    @
+@ GitHub  : https://github.com/FII14/PKSFZ @
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+""")
+
+file_zip = input("Masukkan path ke file zip: ")
+
+if not os.path.exists(file_zip):
+    print(f"Kesalahan: File zip '{file_zip}' tidak ditemukan.")
+    sys.exit(1)
+  
+file_wordlist = input("Masukkan path ke file wordlist: ")
+
+if not os.path.exists(file_wordlist):
+    print(f"Kesalahan: File wordlist '{file_wordlist}' tidak ditemukan.")
+    sys.exit(1)
+
+try:
+    with open(file_wordlist, "r", encoding="utf-8", errors="ignore") as w:
+
+        for kata_sandi in w:
+            kata_sandi = kata_sandi.strip()
+
+            try:
+                with zipfile.ZipFile(file_zip, "r") as z:
+                    z.extractall(pwd=password.encode("utf-8"))
+                print(f"\n[*] File zip: {file_zip}\n[*] Kata sandi: {password}\n[*] Status: Benar\n")
+                sys.exit(0)
+
+            except Exception as e:
+                print(f"\n[*] File zip: {file_zip}\n[*] Kata sandi: {password}\n[*] Status: Salah\n")
+                time.sleep(1)
+
+    print("Kata sandi tidak ditemukan dalam wordlist.")
+    sys.exit(1)
+
+except Exception as e:
+    print(f"Terjadi kesalahan: {e}")
+    sys.exit(1)
