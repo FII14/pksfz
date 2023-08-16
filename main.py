60ab0df7b8c8b386082d100b90f4c5aa263f5b36
FII14 <121723975+FII14@users.noreply.github.com>
2023-08-17 00:40:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,25 +4,32 @@ import time
 import zipfile
 import os
 import sys
+from colorama import Fore
+
+m = Fore.RED
+h = Fore.GREEN
+p = Fore.RESET
+k = Fore.YELLOW
+c = Fore.CYAN
 
 print("""
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-@ Program : Pemecah Kata Sandi File Zip     @
-@ Pembuat : Rofi [FII14]                    @
-@ GitHub  : https://github.com/FII14/PKSFZ  @
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-""")
+{c}@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+{c}@ {p}Program : Pemecah Kata Sandi File Zip {c}    @
+{c}@ {p}Pembuat : Rofi [FII14]  {c}                  @
+{c}@ {p}GitHub  : https://github.com/FII14/PKSFZ {c} @
+{c}@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 
+{p}""")
 
 file_zip = input("Masukkan path ke file zip: ")
 
 if not os.path.exists(file_zip):
-    print(f"Kesalahan: File zip '{file_zip}' tidak ditemukan.")
+    print(f"{m}Kesalahan: File zip '{file_zip}' tidak ditemukan.{p}")
     sys.exit(1)
   
 file_wordlist = input("Masukkan path ke file wordlist: ")
 
 if not os.path.exists(file_wordlist):
-    print(f"Kesalahan: File wordlist '{file_wordlist}' tidak ditemukan.")
+    print(f"{m}Kesalahan: File wordlist '{file_wordlist}' tidak ditemukan.{p}")
     sys.exit(1)
 
 try:
@@ -34,16 +41,16 @@ try:
             try:
                 with zipfile.ZipFile(file_zip, "r") as z:
                     z.extractall(pwd=kata_sandi.encode("utf-8"))
-                print(f"\n[*] File zip: {file_zip}\n[*] Kata sandi: {kata_sandi}\n[*] Status: Benar\n")
+                print(f"\n{h}[*] File zip: {file_zip}\n[*] Kata sandi: {kata_sandi}\n[*] Status: Benar\n{p}")
                 sys.exit(0)
 
             except Exception as e:
-                print(f"\n[*] File zip: {file_zip}\n[*] Kata sandi: {kata_sandi}\n[*] Status: Salah")
+                print(f"\n{m}[*] File zip: {file_zip}\n[*] Kata sandi: {kata_sandi}\n[*] Status: Salah{p}")
                 time.sleep(1)
 
-    print("\nKata sandi tidak ditemukan dalam wordlist.")
+    print(f"\n{k}Kata sandi tidak ditemukan dalam wordlist.{p}")
     sys.exit(1)
 
 except Exception as e:
-    print(f"Terjadi kesalahan: {e}")
+    print(f"{k}Terjadi kesalahan: {e}{p}")
     sys.exit(1)
